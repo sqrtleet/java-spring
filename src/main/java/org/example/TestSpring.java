3e856fbd7785570df7432c62919f7b80f1530c59
sqrtleet <sqrtleet@gmail.com>
2023-12-12 17:00:43 +0900

    gitignore

---
+++ src/main/java/org/example/TestSpring.java
@@ -0,0 +1,19 @@
+package org.example;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class TestSpring {
+    public static void main(String[] args) {
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
+                "applicationContext.xml"
+        );
+
+        Music musicBean = context.getBean("musicBean", Music.class);
+
+        MusicPlayer musicPlayer = new MusicPlayer(musicBean);
+
+        musicPlayer.playMusic();
+
+        context.close();
+    }
+}
